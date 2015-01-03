b7be120ecb8995f33391c726a512a50330f1cd67
Noriaki UCHIYAMA <uchiyama.noriaki@gmail.com>
2015-01-03 21:42:04 +0900

    * refactoring

--- js/glyph-hack-canvas.js
+++ js/glyph-hack-canvas.js
@@ -1,65 +1,129 @@
+// ----- Stage
 var Stage = Class.get({
-    initialize: function(stage) { this.stage = stage; },
+    LAYER_GROUPS: [
+        { name: 'bg', type: 'rectangle' },
+        { name: 'lines', type: 'line' },
+        { name: 'vertexes', type: 'arc' },
+        { name: 'particles', type: 'arc' }
+    ],
+    POINTS: [
+        [320, 80],
+        [80, 218], [560, 218],
+        [200, 287], [440, 287],
+        [320, 356],
+        [200, 425], [440, 425],
+        [80, 494], [560, 494],
+        [320, 632]
+    ],
+    initialize: function(stage) {
+        this.stage = stage;
+        this.Vertexes = new Vertexes(this.stage, this.POINTS);
+        this.BackGround = new BackGround(this.stage);
+        this.Particles = new Particles(this.stage);
+        this.Glyph = new Glyph(this.stage);
+    },
+    setup: function() {
+        var $stage = this.stage;
+        this.BackGround.draw();
+        this.Vertexes.draw();
+        $stage
+            .on('mousedown touchstart', this.Glyph.start_fn())
+            .on('mousedown touchstart', this.Particles.start_fn())
+            .on('mouseup touchend', this.Glyph.end_fn())
+            .on('mouseup touchend', this.Particles.end_fn());
+    },
     rewind: function() {
-        var s = this.stage;
-        function deep_copy(layers) { return $.extend(true, [], layers); }
         $.each([
-            { name: 'bg', type: 'rectangle' },
-            { name: 'lines', type: 'line' },
-            { name: 'vertexes', type: 'arc' },
-            { name: 'particles', type: 'arc' }
-        ], function(i, group) {
-            var shapes = deep_copy(s.getLayerGroup(group.name));
-            s.removeLayerGroup(group.name);
-            $.each(shapes, function(i, shape) {
-                s.draw($.extend(true, { type: group.type }, shape._args));
-            });
-        });
-        s.drawLayers();
-        //var vertexes = deep_copy(s.getLayerGroup('vertexes'));
-        //var particles = deep_copy(s.getLayerGroup('particles'));
+            this.BackGround, this.Glyph, this.Vertexes
+        ], function(i, shapes) { shapes.rewind(); });
     }
 });
-var Line = Class.get({
+
+// ----- Glyph
+var Glyph = Class.get({
     DRAWING: false,
     DRAWN: false,
+    vertexes: [],
+    lines: [],
+    initialize: function(stage) { this.stage = stage; },
+    rewind: function() {
+        this.stage.removeLayerGroup((new Line()).GROUP).drawLayers();
+        $.each(this.lines, function(i, line) {
+            line.draw();
+        });
+    },
+    start_fn: function() {
+        var self = this;
+        return function(e) {
+            return self.start();
+        }
+    },
+    start: function() {
+        return this.DRAWING ? false : this.DRAWING = true;
+    },
+    pass_fn: function() {
+        var self = this;
+        return function(vertex) {
+            var v = $.stage.Vertexes.find(vertex.name);
+            if(self.DRAWING && self.add_vertex(v)) {
+                v.routing = true;
+                var vertexes_length = self.vertexes.length;
+                if(vertexes_length > 1) {
+                    if(self.DRAWN) {
+                        $.stage.Vertexes.reset($.extend([], self.vertexes));
+                        Line.remove_all(self.stage);
+                        self.empty_lines();
+                        self.DRAWN = false;
+                    }
+                    var endpoints = self.vertexes.slice(-2);
+                    var line = new Line(self.stage, endpoints[0], endpoints[1]);
+                    line.draw();
+                    self.lines.push(line);
+                }
+            }
+        };
+    },
+    end_fn: function() {
+        var self = this;
+        return function(e) {
+            return self.end();
+        }
+    },
+    end: function() {
+        this.DRAWING = false;
+        $.stage.rewind();
+        this.DRAWN = true;
+        this.empty_vertexes();
+        return true;
+    },
+    empty_vertexes: function() {
+        while(this.vertexes.length > 0) { this.vertexes.pop(); }
+    },
+    empty_lines: function() {
+        while(this.lines.length > 0) { this.lines.pop(); }
+    },
+    add_vertex: function(vertex) {
+        var vs = this.vertexes;
+        return vs[vs.length-1] === vertex ? false : !!vs.push(vertex);
+    }
+});
+
+// ----- Line
+var Line = Class.get({
     STROKE_COLOR: '#00ffdb',
     STROKE_WIDTH: 18,
     SHADOW_COLOR: '#00ffdb',
     SHADOW_BLUR: 36,
     GROUP: 'lines',
-    stack: [],
-    initialize: function(stage) {
+    initialize: function(stage, start_point, end_point) {
         this.stage = stage;
-    },
-    start: function() {
-        // todo: negative ux by mis-touch
-        //this.stage.removeLayerGroup(this.GROUP).drawLayers();
-        return this.DRAWING ? false : this.DRAWING = true;
-    },
-    end: function() {
-        Particle.remove_all(this.stage);
-        (new Stage(this.stage)).rewind();
-        this.destroy();
-        this.DRAWING = false;
-        this.DRAWN = true;
-    },
-    destroy: function() {
-        while(this.stack.length > 0) { this.stack.pop(); }
-    },
-    add_vertex: function(vertex) {
-        var s = this.stack;
-        return s[s.length-1] === vertex ? false : !!this.stack.push(vertex);
+        this.start_point = start_point;
+        this.end_point = end_point;
     },
     draw: function() {
-        if(this.stack.length < 2) return;
-        var s_vertex = this.stack[0];
-        var e_vertex = this.stack[1];
+        var s_vertex = this.start_point;
+        var e_vertex = this.end_point;
         if(s_vertex !== e_vertex) {
-            if(this.DRAWN) {
-                Line.remove_all(this.stage);
-                this.DRAWN = false;
-            }
             this.stage.drawLine({
                 strokeStyle: this.STROKE_COLOR, strokeWidth: this.STROKE_WIDTH,
                 shadowColor: this.SHADOW_COLOR, shadowBlur: this.SHADOW_BLUR,
@@ -69,14 +133,48 @@ var Line = Class.get({
                 layer: true, groups: [this.GROUP]
             });
         }
-        this.stack.shift();
     }
 });
 Line.remove_all = function(stage) {
-    var layer_group = (new Line(0,0)).GROUP;
+    var layer_group = (new Line()).GROUP;
     return stage.removeLayerGroup(layer_group).drawLayers();
 };
 
+// ----- Vertexes
+var Vertexes = Class.get({
+    items: {},
+    initialize: function(stage, points) {
+        this.stage = stage;
+        var self = this;
+        $.each(points, function(i, point) {
+            var name = 'v' + ('0'+(i+1)).slice(-2);
+            self.items[name] = new Vertex(point[0], point[1], name);
+        });
+    },
+    draw: function() {
+        var self = this;
+        $.each(this.items, function(name, vertex) {
+            vertex.draw(self.stage);
+        });
+    },
+    rewind: function() {
+        this.stage.removeLayerGroup(this.group_name()).drawLayers();
+        this.draw();
+    },
+    reset: function(current_vertexes) {
+        $.each(this.items, function(i, vertex) {
+            var current = false;
+            $.each(current_vertexes, function(j, cv) { if(vertex === cv) current = true; });
+            if(!current) { vertex.routing = false; }
+        });
+        this.rewind();
+    },
+    find: function(name) { return this.items[name]; },
+    group_name: function() { return (new Vertex()).GROUP; },
+    size: function() { return Object.keys(this.items).length; }
+});
+
+// ----- Vertex
 var Vertex = Class.get({
     FILL_COLOR: 'black',
     FILL_COLOR_ROUTE: 'white',
@@ -88,20 +186,14 @@ var Vertex = Class.get({
     SHADOW_BLUR: 45,
     GROUP: 'vertexes',
     routing: false,
-    initialize: function(x, y) {
+    initialize: function(x, y, name) {
         this.x = x;
         this.y = y;
-        this.name = 'v'+x+'x'+y;
+        this.name = (name === undefined ? 'v'+x+'x'+y: name);
     },
     draw: function(stage) {
         var self = this;
-        var handle_mouseover = function(vertex) {
-            if(stage.line.DRAWING) {
-                if(stage.line.add_vertex(vertex)) {
-                    stage.line.draw();
-                }
-            }
-        };
+        var handle_mouseover = $.stage.Glyph.pass_fn();
         stage.drawArc({
             layer: true, name: this.name, groups: [this.GROUP],
             fillStyle: this.fillStyle(),
@@ -114,11 +206,9 @@ var Vertex = Class.get({
                 }, 'fast');
                 handle_mouseover(vertex);
             },
-            //mousedown: handle_mouseover,
             mousedown: function(vertex) {
                 $(stage).trigger('mousedown');
                 handle_mouseover(vertex);
-                //console.log(this, vertex);
             },
             mouseout: function(vertex) {
                 $(this).animateLayer(vertex, {
@@ -131,6 +221,34 @@ var Vertex = Class.get({
         return this.routing ? this.FILL_COLOR_ROUTE : this.FILL_COLOR;
     }
 });
+
+// ----- Particles
+var Particles = Class.get({
+    GROUP: 'particles',
+    initialize: function(stage) {
+        this.stage = stage;
+    },
+    start_fn: function() {
+        var self = this;
+        return function(e) {
+            $(e.target).on('mousemove.particle touchmove.particle', function(e) {
+                (new Particle(e.offsetX, e.offsetY)).draw(self.stage);
+            });
+        }
+    },
+    end_fn: function() {
+        var self = this;
+        return function(e) {
+            $(e.target).off('.particle');
+            self.remove_all();
+        }
+    },
+    remove_all: function() {
+        return this.stage.removeLayerGroup(this.GROUP).drawLayers();
+    }
+});
+
+// ----- Particle
 var Particle = Class.get({
     RAND_XY_RANGE: 8,
     RAND_RADIUS_RANGE: 5,
@@ -157,58 +275,27 @@ var Particle = Class.get({
         });
     }
 });
-Particle.remove_all = function(stage) {
-    var layer_group = (new Particle(0,0)).GROUP;
-    return stage.removeLayerGroup(layer_group).drawLayers();
-};
 
+// ----- BackGround
 var BackGround = Class.get({
     COLOR: 'black',
     GROUP: 'bg',
-    initialize: function() {},
-    draw: function(stage) {
+    initialize: function(stage) { this.stage = stage; },
+    draw: function() {
+        var stage = this.stage;
         stage.drawRect({
             fillStyle: this.COLOR, x: 0, y:0, fromCenter: false,
             width: stage.width(), height: stage.height(),
             layer: true, name: 'bg', group: [this.GROUP]
         });
+    },
+    rewind: function() {
+        this.stage.removeLayerGroup(this.GROUP).drawLayers();
+        this.draw();
     }
 });
 
 jQuery(function($) {
-    var $stage = $('#stage');
-    $stage.line = new Line($stage);
-    var points = [
-        [320, 80],
-        [80, 218], [560, 218],
-        [200, 287], [440, 287],
-        [320, 356],
-        [200, 425], [440, 425],
-        [80, 494], [560, 494],
-        [320, 632]
-    ];
-    var bg = new BackGround();
-    bg.draw($stage);
-    $.each(points, function(i,point) {
-        var vertex = new Vertex(point[0], point[1]);
-        vertex.draw($stage);
-    });
-
-    $stage.on('mousedown touchstart', function(e) {
-        if($stage.line.start()) {
-            //console.log('start',e);
-            $(e.target).on('mousemove.glyph touchmove.glyph', function(e) {
-                //console.log('move',e);
-                var particle = new Particle(e.offsetX, e.offsetY);
-                particle.draw($stage);
-            });
-        }
-    }).on('mouseup touchend', function(e) {
-        $(e.target).off('.glyph');
-        $stage.line.end();
-        //console.log('end',e);
-    });
-
-
-    window.stage = new Stage($stage);
+    $.stage = new Stage($('#stage'));
+    $.stage.setup();
 });
